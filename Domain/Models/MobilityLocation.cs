3a563843ea4a9dad288869dba2827fc82c246b7f
Lemonada10 <firas.haddad.h@gmail.com>
2026-03-04 17:18:01 -0500

    T-10 completed. The reservations are now saved in the database, and the user can reserve the spots as long as the available seats are above 0.

--- Domain/Models/MobilityLocation.cs
+++ Domain/Models/MobilityLocation.cs
@@ -1,5 +1,6 @@
-﻿﻿﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace SUMMS.Api.Domain.Models;
 
@@ -25,6 +26,7 @@ public class MobilityLocation
     
     public int AvailableSpots { get; set; }
 
+    [JsonIgnore]
     public ICollection<Reservation> Reservations { get; set; } = new List<Reservation>();
 }
 
